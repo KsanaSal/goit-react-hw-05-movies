e158ddf2a9a0fa0ff61ef380bc47d8458cb9897a
KsanaSal <oasalivon@gmail.com>
2022-11-06 16:44:35 +0200

    Fixed app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 
 const Home = lazy(() => import('../pages/Home'));
@@ -18,7 +18,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
